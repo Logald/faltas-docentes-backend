ea3f8db7b7e7432efeb1807f62453771456681db
sofelixx <s18paris@gmail.com>
2023-04-14 14:14:04 +0000

    the error in TurnSeeder was fixed

--- database/seeders/TurnSeeder.php
+++ database/seeders/TurnSeeder.php
@@ -10,9 +10,9 @@ class TurnSeeder extends Seeder
   public function run()
   {
     $turns = [
-      ['name' => 'Matutino', 'start_time' => '08:00:00', 'end_time' => '13:00:00'],
-      ['name' => 'Vespertino', 'start_time' => '14:00:00', 'end_time' => '19:00:00'],
-      ['name' => 'Nocturno', 'start_time' => '20:00:00', 'end_time' => '00:00:00'],
+      ['name' => 'Matutino'],
+      ['name' => 'Vespertino'],
+      ['name' => 'Nocturno'],
     ];
     foreach ($turns as $turn) {
       Turn::firstOrCreate($turn);
