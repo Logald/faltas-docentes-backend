0209f383cd2aa3f845cb4696fc0626f6491cc784
sofelix <s18paris@gmail.com>
2023-05-24 04:03:58 +0000

    the limit of request was changed to 600 per minute

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,12 +36,14 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':600,1'
         ],
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':600,1'
         ],
     ];
 
